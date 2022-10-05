9a9e2ecfcd71b871997c2bb500d8d66b87007c4b
nickatnight <kay.muneee@gmail.com>
2022-10-04 22:06:29 -0700

    fix: meh

--- tag_youre_it/repository/player.py
+++ tag_youre_it/repository/player.py
@@ -12,14 +12,16 @@ logger: logging.Logger = logging.getLogger(__name__)
 
 
 class PlayerRepository(AbstractRepository[Player, IPlayerCreate, IPlayerUpdate]):
+    model = Player
+
     async def list_opted_out(self) -> List[Player]:
         statement = select(self.model).where(self.model.opted_out is True)
-        results = self.db.exec(statement)
+        results = self.db.execute(statement)
         return results.scalars().all()
 
     async def set_opted_out(self, reddit_id: str) -> List[Player]:
         statement = select(self.model).where(self.model.reddit_id == reddit_id)
-        results = self.db.exec(statement)
+        results = self.db.execute(statement)
         player: Player = results.one()
 
         player.opted_out = True
--- tests/unit/tag_youre_it/core/test_engine.py
+++ tests/unit/tag_youre_it/core/test_engine.py
@@ -1,2 +1,5 @@
+# from tag_youre_it.core.engine import Engine
+
+
 def test_engine_simple():
     assert "hi" == "hi"
